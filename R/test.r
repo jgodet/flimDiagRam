838aa65709e73dec4e5e5f0ee861fc5e74add776
jgodet <jgodet@unistra.fr>
2019-07-25 11:29:37 +0200

    c

---
+++ R/test.r
@@ -0,0 +1,21 @@
+# test.r
+# written by JuG
+# July 25 2019
+
+
+#' Do something
+#' @author JuG
+#' @description 
+#' @param 
+#' @details 
+#' @examples 
+#'
+#'
+#' @return 
+#' @export
+
+
+test<- function(){
+
+  return()
+}

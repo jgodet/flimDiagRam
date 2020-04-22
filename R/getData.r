36977c15a5e80bce92a527a39f58724b3da1d7d8
jgodet <jgodet@unistra.fr>
2020-04-22 12:50:18 +0200

    add getData

--- R/getData.r
+++ R/getData.r
@@ -3,19 +3,28 @@
 # December 12 2019
 
 
-#' Do something
+#' Read single exponential decay data file
 #' @author JuG
-#' @description 
-#' @param 
+#' @description Read single exponential decay data file
+#' @param pathTau path to the lifetime data file (*_t1.asc)
+#' @param pathTau path to the lifetime data file (*_photons.asc)
+#' @param label
 #' @details 
 #' @examples 
 #'
 #'
-#' @return 
+#' @return data frame 
 #' @export
 
 
-getData <- function(){
-  
-  return()
-}
+getData <- function(pathTau, pathPhoton, label){
+
+    tau1d <- read.table(pathTau,h=FALSE)
+    tau1d <- as.vector(as.matrix(tau1d))
+    photons <- read.table(pathPhoton,h=FALSE)
+    photons <- as.vector(as.matrix(photons))
+    
+    dataD <- data.frame(tau1d, photons)
+    dataD$ind <- label
+    return(dataD)
+  }

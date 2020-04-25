026bc1b6499732b687a143a6921343f7b18bf4aa
jgodet <jgodet@unistra.fr>
2020-04-25 23:28:03 +0200

    add CI edcf

--- R/plotEcdf.r
+++ R/plotEcdf.r
@@ -8,6 +8,9 @@
 #' @description Plot empirical cumulative distribution functions
 #' @param AA dataset a
 #' @param BB dataset b
+#' @param col1 color 1
+#' @param col2 color 2
+#' @param CI boolean plot confidence interval
 #' @details 
 #' 
 #' @examples 
@@ -17,7 +20,7 @@
 #' @export
 
 
-plotEcdf <- function(AA,BB,col1, col2,...){
+plotEcdf <- function(AA,BB,col1, col2,CI=FALSE, ...){
   if(missing(col1)){
     col1 = rgb(.153,.392,.482)
   }
@@ -27,9 +30,34 @@ plotEcdf <- function(AA,BB,col1, col2,...){
   xy <- ecdf(AA)
   xx <- get("x", envir=environment(xy))# = sort(x)
   yy <- get("y", envir=environment(xy))
+  n = length(yy)
   plot(xx,yy,col=col1,lwd=2,type='l',...)
-  xy <- ecdf(BB)
-  xx <- get("x", envir=environment(xy))# = sort(x)
-  yy <- get("y", envir=environment(xy))
-  lines(xx,yy,col=col2,lwd=2,...)
-}
+  xy1 <- ecdf(BB)
+  n1 = length(xy1)
+  xx1 <- get("x", envir=environment(xy1))# = sort(x)
+  yy1 <- get("y", envir=environment(xy1))
+  lines(xx1,yy1,col=col2,lwd=2,...)
+
+  if(CI=TRUE){
+    
+    # Dvoretzky–Kiefer–Wolfowitz inequality:
+    # P ( sup|F_n - F| > epsilon  ) leq 2*exp(-2n*epsilon^2)
+    # set alpha to 0.05 and alpha=2*exp(-2n*epsilon^2):
+    # --> epsilon_n = sqrt(-log(0.5*0.05)/(2*n))
+    
+    
+    for (i in c(n,n1)){
+      n <- i
+      #lower and upper bands:
+      L<-1:n
+      U<-1:n
+      epsilon_i = sqrt(log(2/0.05)/(2*n))
+      
+      L=pmax(1:n/n-epsilon_i, 0)
+      U=pmin(1:n/n+epsilon_i, 1)
+      polygon(x = c(xx,rev(xx)), y = c(U, rev(L)),col = paste(col1,"80", sep=''), border = NA)
+      }
+  
+  }
+  
+  }
